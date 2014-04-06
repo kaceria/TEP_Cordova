645cafb78c307e450d757021288c9745b930e10d
Asan Kaceri <asan.kaceri@ikr.de>
2014-04-06 20:28:41 +0200

    Create DB  and Table for Pref

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -28,8 +28,54 @@ var app = {
 
 $(document).on('deviceready', function() {
 
-	gaslogin.check();
-   app.init();
-  
+    var db = window.openDatabase("test", "1.0", "Test DB", 1000000);
+    db.transaction(Del_DB, errorCB, successCB);
+    db.transaction(populateDB, errorCB, successCB);
+
 });
 
+
+
+document.addEventListener("offline", onOffline, false);
+
+function onOffline() {
+
+    app.init();
+
+
+}
+
+
+document.addEventListener("online", onOnline, false);
+
+function onOnline() {
+
+    gaslogin.check();
+    app.init();
+}
+
+function populateDB(tx) {
+
+    tx.executeSql('CREATE TABLE IF NOT EXISTS PREF (id unique, Status , Userid , Name , Vorname)');
+    tx.executeSql('SELECT * FROM PREF WHERE id = 1',[],function(tb,res){
+
+        if(res.rows.length = 0){
+
+            tb.executeSql('INSERT INTO PREF (id, data) VALUES (1, 0)');
+        }
+
+    },errorCB);
+}
+
+
+function Del_DB(tx){
+    tx.executeSql('DROP TABLE IF EXISTS PREF');
+}
+
+function errorCB(err) {
+    alert("Error processing SQL: "+err);
+}
+
+function successCB() {
+    alert("success!");
+}
